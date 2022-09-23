73ce8ac349e50bbab72eb033edf31e6665b11571
ngo.hoang.long <SUN-ASTERISK\ngo.hoang.long@b123165-pc.sun-asterisk.com>
2022-09-23 13:54:35 +0700

    task 52980 next user to other user

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     root "landing_pages#home"
     post "/login", to: "sessions#create"
-    get "/match", to: "match_pages#match"
+    get "/match", to: "match_pages#index"
     get "/admin", to: "admin_pages#index"
     get "/signup", to: "users#new"
     delete "/logout", to: "sessions#destroy"
+    get "/next", to: "match_pages#next"
     resources :users
     resources :relationships, only: %i(create destroy)
     resources :match_pages, only: %i(create)
