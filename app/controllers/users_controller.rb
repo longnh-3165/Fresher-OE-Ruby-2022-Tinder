8419388a94cde43d0f61a6857ce9cb3f1215456e
ngo.hoang.long <SUN-ASTERISK\ngo.hoang.long@b123165-pc.sun-asterisk.com>
2022-09-24 10:43:44 +0700

    rpec user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,7 +44,7 @@ class UsersController < ApplicationController
     return if @user
 
     flash[:danger] = t ".not_found"
-    redirect_to user_path(current_user)
+    render "shared/_not_found"
   end
 
   def correct_user
